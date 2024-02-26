bad71545ce29cf07c87b5c623856fb7b0366007a
Manesh Timilsina <smanesh2004@gmail.com>
2024-02-26 11:35:32 +0545

    Update donate link

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ if ( !defined( 'ABSPATH' ) ) {
 	exit( 'Direct script access denied.' );
 }
 
-define( 'ZINO_VERSION', '1.0.9' );
+define( 'ZINO_VERSION', '1.1.0' );
 define( 'ZINO_DIR', rtrim( get_template_directory(), '/' ) );
 define( 'ZINO_URI', rtrim( get_template_directory_uri(), '/' ) );
 
@@ -70,7 +70,7 @@ function zino_add_admin_notice() {
 add_action( 'admin_init', 'zino_add_admin_notice' );
 
 function zino_add_donate_link() {
-	echo '<span style="font-weight: bold;"><a href="https://www.buymeacoffee.com/maneshtimilsina" target="_blank">Buy Me a Coffee</a></span>';
+	echo '<span style="font-weight: bold;"><a href="https://ko-fi.com/maneshtimilsina" target="_blank">Buy Me a Coffee</a></span>';
 }
 
 add_action( 'zino_after_admin_notice_link_items', 'zino_add_donate_link' );
